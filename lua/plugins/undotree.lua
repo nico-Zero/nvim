9173ccaca9adb6bb0008765494e7abe513c52b26
nico-Zero <zandaxheart955@gmail.com>
2024-09-27 18:19:11 +0530

    update.
    M  lazy-lock.json
    R  lua/plugins/treesitter.lua -> lua/plugins/nvim_treesitter.lua
    M  lua/plugins/nvim_treesitter_textobject.lua
    M  lua/plugins/undotree.lua

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,6 @@
-return{
-    "mbbill/undotree",
-    config = function ()
-        vim.keymap.set( "n", "<F5>", "<cmd>UndotreeToggle<CR>", { noremap = true, silent = true })
-
-    end,
+return {
+	"mbbill/undotree",
+	config = function()
+		vim.keymap.set("n", "<F5>", "<cmd>UndotreeToggle<CR>", { noremap = true, silent = true })
+	end,
 }
