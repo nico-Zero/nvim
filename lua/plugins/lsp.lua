2a0b361d9eadee14838fc3369e60f5737369e539
nico-Zero <zandaxheart955@gmail.com>
2024-09-28 20:28:51 +0530

    update.
    M  lua/plugins/lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -161,27 +161,8 @@ return {
             require("lsp_lines").setup()
 
             vim.diagnostic.config({
-                virtual_text = true,
-                virtual_lines = false,
-            })
-            -- Function to enable lsp_lines only in file buffers
-            local function enable_lsp_lines_for_files()
-                if vim.fn.bufname('%') ~= '' then -- Check if the buffer is associated with a file
-                    vim.diagnostic.config({
-                        virtual_text = false,
-                        virtual_lines = true,
-                    })
-                else
-                    vim.diagnostic.config({
-                        virtual_text = true,
-                        virtual_lines = false,
-                    })
-                end
-            end
-
-            -- Trigger the function on BufEnter and FileType events
-            vim.api.nvim_create_autocmd({ 'BufEnter', 'FileType' }, {
-                callback = enable_lsp_lines_for_files,
+                virtual_text = false,
+                virtual_lines = true,
             })
         end
     },
