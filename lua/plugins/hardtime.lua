c1658e18edaab13a42cbb76759f13858d1d0a499
nico-Zero <zandaxheart955@gmail.com>
2024-10-01 16:23:28 +0530

    update.
    M  lazy-lock.json
    D  lua/plugins/hardtime.lua

--- lua/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return {
-    -- lazy.nvim
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {},
-    config = function()
-        require("hardtime").setup()
-    end
-}
