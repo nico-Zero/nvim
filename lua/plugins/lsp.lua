f27137183493ebd5a032c70614369575cf8f967d
nico-Zero <zandaxheart955@gmail.com>
2024-10-01 07:46:07 +0530

    udpate.
    M  lazy-lock.json
    M  lua/config/keymaps.lua
    M  lua/plugins/conform.lua
    A  lua/plugins/hartime.lua
    D  lua/plugins/iron.lua
    M  lua/plugins/lsp.lua
    D  lua/plugins/toggleterm.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -177,7 +177,6 @@ return {
         "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
         config = function()
             require("lsp_lines").setup()
-
             vim.diagnostic.config({
                 virtual_text = false,
                 signs = true,
