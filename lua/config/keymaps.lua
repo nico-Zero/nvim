7a40c70902ce29b0cf6fd9b16246a5ac1a9bd9b7
nico-Zero <zandaxheart955@gmail.com>
2024-10-04 22:22:35 +0530

    .
    R  _init.lua -> kickstart_init.lua
    M  lua/config/keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,26 +21,26 @@ map("n", "Y", 'v$"+y', { noremap = true, silent = true })
 map("n", "P", "Vp", { noremap = true, silent = true })
 map("n", "<M-bs>", "diw", { noremap = true, silent = true })
 map("n", "gcp", "vipgc", { noremap = true, silent = true })
-map('n', '<leader>tw', ':set wrap!<CR>', { noremap = true, silent = true })
+map("n", "<leader>tw", ":set wrap!<CR>", { noremap = true, silent = true })
 
 function ToggleBQF()
-    -- Get the ID of the quickfix window (0 if it’s not open)
-    local qf_winid = vim.fn.getqflist({ winid = 0 }).winid
-    if qf_winid ~= 0 then
-        -- Quickfix window is open; close it
-        vim.cmd('cclose')
-    else
-        -- Quickfix window is not open; open it if there are entries
-        if not vim.tbl_isempty(vim.fn.getqflist()) then
-            vim.cmd('copen')
-        else
-            print("Quickfix list is empty.")
-        end
-    end
+	-- Get the ID of the quickfix window (0 if it’s not open)
+	local qf_winid = vim.fn.getqflist({ winid = 0 }).winid
+	if qf_winid ~= 0 then
+		-- Quickfix window is open; close it
+		vim.cmd("cclose")
+	else
+		-- Quickfix window is not open; open it if there are entries
+		if not vim.tbl_isempty(vim.fn.getqflist()) then
+			vim.cmd("copen")
+		else
+			print("Quickfix list is empty.")
+		end
+	end
 end
 
-map('n', '<M-n>', '<cmd>cnext<CR>zz', { noremap = true, silent = true })
-map('n', '<M-p>', '<cmd>cprev<CR>zz', { noremap = true, silent = true })
-map('n', '<leader>qq', ToggleBQF, { noremap = true, silent = true })
-map('n', '<leader>qn', '<cmd>cnewer<CR>', { noremap = true, silent = true })
-map('n', '<leader>qp', '<cmd>colder<CR>', { noremap = true, silent = true })
+map("n", "<M-n>", "<cmd>cnext<CR>zz", { noremap = true, silent = true })
+map("n", "<M-p>", "<cmd>cprev<CR>zz", { noremap = true, silent = true })
+map("n", "<leader>qq", ToggleBQF, { noremap = true, silent = true })
+map("n", "<leader>qn", "<cmd>cnewer<CR>", { noremap = true, silent = true })
+map("n", "<leader>qp", "<cmd>colder<CR>", { noremap = true, silent = true })
