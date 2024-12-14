b18dd47be88f7377f0f517d1d856ca5ce48af344
nico-Zero <zandaxheart955@gmail.com>
2024-12-14 21:24:43 +0530

    update...
    M  lazy-lock.json
    M  lua/plugins/harpoon.lua

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,7 +3,10 @@ return {
     config = function()
         require("harpoon").setup()
         require("telescope").load_extension('harpoon')
-        vim.keymap.set("n", "<leader>a", ":lua require('harpoon.mark').add_file()<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>a", function()
+            require("harpoon.mark").add_file()
+            vim.notify("File added to Harpoon!", vim.log.levels.INFO, { title = "Harpoon" })
+        end, { noremap = true, silent = true })
         vim.keymap.set("n", "<C-e>", ":lua require('harpoon.ui').toggle_quick_menu()<CR>",
             { noremap = true, silent = true })
         -- vim.keymap.set("n","<C-t>",":Telescope harpoon marks<CR>", { noremap = true, silent = true })
